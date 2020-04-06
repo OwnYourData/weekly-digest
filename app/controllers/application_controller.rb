8d36b0816923f1a27adf6635b34d2c17e0b303e3
Christoph Fabianek <christoph.fabianek@gmail.com>
2020-04-07 00:50:59 +0200

    basic user management and RSS feed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	before_action :set_locale
+  	include SessionsHelper
 
 	def default_url_options(options={})
 		I18n.locale == :en ? {} : { locale: I18n.locale }
