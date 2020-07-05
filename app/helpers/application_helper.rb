69749aabb9b63249062d81bfcfcb2b5edf1fee86
Christoph Fabianek <christoph.fabianek@gmail.com>
2020-07-05 22:49:59 +0200

    backup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -47,6 +47,79 @@ module ApplicationHelper
         else
             return number
         end
-    end    
+    end
 
+    def getWeekCount(weekly_id, lang)
+        info_cnt = 0
+        quest_cnt = 0
+        app_cnt = 0
+        mydata_cnt = 0
+
+        @week = Weekly.find(weekly_id)
+        if !@week.nil?
+            @it = WeeklyInternal.where(weekly_id: weekly_id, lang: lang)
+            lang_array = ["", nil, lang]
+            if lang == "en"
+                intro_text = @week["intro"].to_s
+            else
+                if @it.count > 0
+                    intro_text = @it.first.intro.to_s
+                    if @it.first.locale_only
+                        lang_array = lang
+                    end
+                else
+                    intro_text = @week["intro"].to_s
+                end
+            end
+            info_cnt = @week.posts.where(category: "info").where(lang: lang_array).count rescue 0
+            quest_cnt = @week.posts.where(category: "question").where(lang: lang_array).count rescue 0
+            app_cnt = @week.weekly_apps.count rescue 0
+            mydata_cnt = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true).render(intro_text).scan(/<li>/).length rescue 0
+        end
+        return info_cnt, quest_cnt, app_cnt, mydata_cnt
+    end
+
+    def getWeekCountText(weekly_id, lang, text_style = "default")
+        retVal = ""
+        info_cnt, quest_cnt, app_cnt, mydata_cnt = getWeekCount(weekly_id, lang)
+
+        case text_style
+        when "social"
+            info_str = 'news.og_desc_post'
+            quest_str = 'news.og_desc_quest'
+            app_str = 'news.og_desc_tool'
+            mydata_str = 'news.talking_point'
+        else
+            info_str = 'news.post'
+            quest_str = 'news.question'
+            app_str = 'news.tool'
+            mydata_str = 'news.talking_point'
+        end
+
+        txt_pieces = []
+        if info_cnt > 0
+            txt_pieces << info_cnt.to_s + " " + t(info_str, count: info_cnt)
+        end
+        if quest_cnt > 0
+            txt_pieces << quest_cnt.to_s + " " + t(quest_str, count: quest_cnt)
+        end
+        if app_cnt > 0
+            txt_pieces << app_cnt.to_s + " " + t(app_str, count: app_cnt)
+        end
+        if mydata_cnt > 0
+            txt_pieces << mydata_cnt.to_s + " " + t(mydata_str, count: mydata_cnt)
+        end
+
+        case txt_pieces.length
+        when 0
+            retVal = ""
+        when 1
+            retVal = txt_pieces.first
+        when 2
+            retVal = txt_pieces.first + " " + t('news.og_desc_and') + " " + txt_pieces.last
+        else
+            retVal = txt_pieces.first(txt_pieces.length - 2).join(", ") + ", " + txt_pieces.last(2).first + " " + t('news.og_desc_and') + " " + txt_pieces.last
+        end
+        return retVal
+    end
 end
